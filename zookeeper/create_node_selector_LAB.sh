3b7a4fb611364d10ee05a92f8b751d7c77c1ac79
commoninfrastructure <johann.reichl@atos.net>
2017-06-09 15:30:23 +0200

    Create create_node_selector_LAB.sh

---
+++ zookeeper/create_node_selector_LAB.sh
@@ -0,0 +1,4 @@
+
+oc label node oshift-minion1.it-solutions.myatos.net broker=app-cikafka-v1
+oc label node oshift-minion2.it-solutions.myatos.net broker=app-cikafka-v2
+oc label node oshift-minion3.it-solutions.myatos.net broker=app-cikafka-v3
