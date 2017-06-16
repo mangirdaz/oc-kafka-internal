404b6a3f7d05809f820a4172a70acbd374481ada
commoninfrastructure <johann.reichl@atos.net>
2017-06-16 15:36:38 +0200

    Update create_cm_zk.sh

--- zookeeper/create_cm_zk.sh
+++ zookeeper/create_cm_zk.sh
@@ -1,3 +1,5 @@
 
 oc create configmap zk-config --from-file=zookeeper.properties
-
+oc create configmap hawkular-openshift-agent-zookeeper-1 --from-file=hawkular-openshift-agent-zookeeper-1-configmap.yaml
+oc create configmap hawkular-openshift-agent-zookeeper-2 --from-file=hawkular-openshift-agent-zookeeper-2-configmap.yaml
+oc create configmap hawkular-openshift-agent-zookeeper-3 --from-file=hawkular-openshift-agent-zookeeper-3-configmap.yaml
