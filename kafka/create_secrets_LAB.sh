10d82fa74082dd37c2e3deaca47fb0fece7b9872
commoninfrastructure <johann.reichl@atos.net>
2017-06-16 16:41:56 +0200

    Delete create_secrets_LAB.sh

--- kafka/create_secrets_LAB.sh
+++
@@ -1,4 +0,0 @@
-
-oc create secret generic kafka-broker-1-tls --from-file=broker-1.keystore.jks --from-file=broker-1.truststore.jks
-oc create secret generic kafka-broker-2-tls --from-file=broker-2.keystore.jks --from-file=broker-2.truststore.jks
-oc create secret generic kafka-broker-3-tls --from-file=broker-3.keystore.jks --from-file=broker-3.truststore.jks
